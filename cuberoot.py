6d2d79bae6fc5d1a5a2b354900f611fdac9549b9
Glenn Sun <Glenn Sun>
2016-12-31 19:44:55 -0500

    Cube root structure

--- cuberoot.py
+++ cuberoot.py
@@ -1,1 +1,14 @@
 # Verify that 2017 is the smallest number whose cube root begins with 10 distinct digits
+
+from __future__ import division
+
+def cube_root(input, precision=10):
+	pass
+
+def contains_all_digits(input):
+	pass
+
+for i in range(2018):
+	result = cube_root(i)
+	if contains_all_digits(result):
+		print i, result
