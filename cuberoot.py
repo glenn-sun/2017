150acd1ca8ba71d9e89dffbda4be726e4eeb5281
Glenn Sun <Glenn Sun>
2016-12-31 19:49:36 -0500

    Cube root complete

--- cuberoot.py
+++ cuberoot.py
@@ -3,10 +3,13 @@
 from __future__ import division
 
 def cube_root(input, precision=10):
-	pass
+	return str(input ** (1 / 3))[:precision + 1]
 
 def contains_all_digits(input):
-	pass
+	for i in [str(n) for n in range(10)]:
+		if i not in input:
+			return False
+	return True
 
 for i in range(2018):
 	result = cube_root(i)
